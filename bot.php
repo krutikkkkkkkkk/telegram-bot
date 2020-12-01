87c6279563d58eff043d2a35672b943e67a3fa08
Krutik <58563133+reboot13-git@users.noreply.github.com>
2020-12-01 21:19:40 +0530

    Update bot.php

--- bot.php
+++ bot.php
@@ -362,14 +362,14 @@ else {
 
      ///Send Message (Global)
     function send_message($chat_id, $message){
-        $apiToken =  $_ENV["API_TOKEN"]
+        $apiToken =  "API_TOKEN";
         $text = urlencode($message);
         file_get_contents("https://api.telegram.org/bot$apiToken/sendMessage?chat_id=$chat_id&reply_to_message_id=$message_id&text=$text");
     }
     
 //Send Messages with Markdown (Global)
       function send_MDmessage($chat_id, $message){
-       $apiToken =  $_ENV["API_TOKEN"]
+       $apiToken =  "API_TOKEN";
         $text = urlencode($message);
         file_get_contents("https://api.telegram.org/bot$apiToken/sendMessage?chat_id=$chat_id&text=$message&parse_mode=Markdown");
     }
@@ -377,7 +377,7 @@ else {
 
 ///Send Message to Channel
       function send_Cmessage($channel_id, $message){
-       $apiToken =  $_ENV["API_TOKEN"]
+       $apiToken =  "API_TOKEN";
         $text = urlencode($message);
         file_get_contents("https://api.telegram.org/bot$apiToken/sendMessage?chat_id=$channel_id&text=$text");
     }
