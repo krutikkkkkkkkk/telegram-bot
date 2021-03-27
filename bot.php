3bc4248dcb9feb1de374c284c528123684c9a39b
Krutik Raut <58563133+reboot13-git@users.noreply.github.com>
2021-03-27 14:22:34 +0530

    Update bot.php

--- bot.php
+++ bot.php
@@ -476,14 +476,14 @@ Checked By @$username ***");
 ///Send Message (Global)
     function send_message($chat_id,$message_id, $message){
         $text = urlencode($message);
-        $apiToken = $_ENV['BOT_TOKEN'];  
+        $apiToken = $_ENV['API_TOKEN'];  
         file_get_contents("https://api.telegram.org/bot$apiToken/sendMessage?chat_id=$chat_id&reply_to_message_id=$message_id&text=$text");
     }
     
 //Send Messages with Markdown (Global)
       function send_MDmessage($chat_id,$message_id, $message){
         $text = urlencode($message);
-        $apiToken = $_ENV['BOT_TOKEN'];  
+        $apiToken = $_ENV['API_TOKEN'];  
         file_get_contents("https://api.telegram.org/bot$apiToken/sendMessage?chat_id=$chat_id&reply_to_message_id=$message_id&text=$text&parse_mode=Markdown");
     }
 ///Send Message to Channel
@@ -495,7 +495,7 @@ Checked By @$username ***");
 
 //Send Dice (dynamic emoji)
 function sendDice($chat_id,$message_id, $message){
-        $apiToken = $_ENV['BOT_TOKEN'];  
+        $apiToken = $_ENV['API_TOKEN'];  
         file_get_contents("https://api.telegram.org/bot$apiToken/sendDice?chat_id=$chat_id&reply_to_message_id=$message_id&text=$message");
     }
 
