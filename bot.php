012e36705c79655e143296c2b3cdfcbfb81511b2
Krutik Raut <58563133+reboot13-git@users.noreply.github.com>
2021-10-05 09:15:53 +0530

    New Command /rate {cryptoName}
    New command to check crypto rate in USD for all coinbase supported Crypto

--- bot.php
+++ bot.php
@@ -300,6 +300,43 @@ else {
 }
     }
 
+
+if (strpos($message, "/rate") === 0) {
+    $coinName = substr($message, 6);
+    $coin = strtoupper($coinName);
+    $curl = curl_init();
+    curl_setopt_array($curl, [
+ CURLOPT_URL => "https://api.coinbase.com/v2/prices/$coin-USD/spot",
+     CURLOPT_RETURNTRANSFER => true,
+     CURLOPT_FOLLOWLOCATION => true,
+     CURLOPT_ENCODING => "",
+     CURLOPT_MAXREDIRS => 10,
+     CURLOPT_TIMEOUT => 50,
+     CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+     CURLOPT_CUSTOMREQUEST => "GET",
+     CURLOPT_HTTPHEADER => [
+         "accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+         "accept-encoding: gzip, deflate, br",
+         "accept-language: en-IN,en-GB;q=0.9,en-US;q=0.8,en;q=0.7", 
+ "user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36"
+   ],
+ ]);
+ $value = curl_exec($curl);
+ curl_close($curl);
+ $currentValue = json_decode($value, true);
+ 
+ $valueinUSD = $currentValue["data"]["amount"];
+ if($valueinUSD != null){
+    send_MDmessage($chat_id,$message_id, "***1 $coin \nUSD = $valueinUSD $ \nRate checked by @$username ***");
+
+ }
+ else{
+    send_MDmessage($chat_id,$message_id, "***Invalid Crypto Name. checked by @$username ***");
+
+ }   
+}
+
+
  /// BTC rate
 if(strpos($message, "/btcrate") === 0){
    $curl = curl_init();
